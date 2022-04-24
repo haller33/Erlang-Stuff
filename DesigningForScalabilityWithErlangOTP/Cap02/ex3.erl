05c8baa547e2ad1cebcf92a7c8506f561f3bf70e
haller33 <eliseu42lucena@gmail.com>
2022-04-24 13:53:33 -0300

    exercise of book

---
+++ DesigningForScalabilityWithErlangOTP/Cap02/ex3.erl
@@ -0,0 +1,17 @@
+-module(ex3).
+-export([filter/2, is_even/1]).
+
+filter(_, []) ->
+    [];
+filter(P, [X|Xs]) ->
+    case P(X) of
+	true -> 
+	    [X|filter(P, Xs)];
+	_ -> 
+	    filter(P,Xs)
+    end.
+
+
+
+is_even(X) ->
+    X rem 2 == 0.
