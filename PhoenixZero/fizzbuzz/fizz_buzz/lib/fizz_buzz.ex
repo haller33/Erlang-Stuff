abeceb046fe7655b25f730a8202edd123545dc8b
haller33 <amnesia1101101@gmail.com>
2022-04-24 10:02:59 -0300

    alterações de refatoração e todo list

--- PhoenixZero/fizzbuzz/fizz_buzz/lib/fizz_buzz.ex
+++ PhoenixZero/fizzbuzz/fizz_buzz/lib/fizz_buzz.ex
@@ -1,58 +1,24 @@
 defmodule FizzBuzz do
-  
   use Witchcraft
 
-  def handle_file_split_inline({:ok, result}) do
-    result
-    |> String.replace("\n", "")
-    |> String.split(",")
-    # |> Enum.map(fn number -> String.to_integer(number) end)
-    # simplify expression
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&convert_and_eval_numbers/1)
+  defp handle_file_split_inline({:ok, result}) do
+    result =
+      result
+      |> String.replace("\n", "")
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.map(&evaluate_numbers/1)
 
-    # "1,2,3,4" => ["1","2","3","4"]
+    {:ok, result}
   end
 
-  def handle_file_split_inline({:error, reason}), do: "error read file: #{reason}"
-
-  def handle_file_read_inline({:ok, result}), do: result
-  def handle_file_read_inline({:error, reason}), do: reason
-
-  def handle_file(file_result) do
-    case file_result do
-      {:ok, result} -> result
-      {:error, reason} -> reason
-    end
-  end
+  defp handle_file_split_inline({:error, reason}), do: {:error, "error read file: #{reason}"}
 
   # using Guards
-  def evaluate_numbers(num) when rem(num, 5) == 0 and rem(num, 3) == 0, do: :fizzbuzz
-  def evaluate_numbers(num) when rem(num, 3) == 0, do: :fizz
-  def evaluate_numbers(num) when rem(num, 5) == 0, do: :buzz
-  def evaluate_numbers(num), do: num
-
-  def convert_and_eval_numbers(element) do
-    element
-    |> evaluate_numbers()
-
-    # if rem(number, 3) == 0 do
-    #   :fizz
-    # else if (rem(number, 5) == 0) do
-    #   :buzz
-    # end
-    # end
-
-    # case rem(number, 3) do
-    #   0 -> :fizz
-    #   _ -> number
-    # end
-
-    # case rem(number, 5) do
-    #   0 -> :buzz
-    #   _ -> number
-    # end
-  end
+  defp evaluate_numbers(num) when rem(num, 5) == 0 and rem(num, 3) == 0, do: :fizzbuzz
+  defp evaluate_numbers(num) when rem(num, 3) == 0, do: :fizz
+  defp evaluate_numbers(num) when rem(num, 5) == 0, do: :buzz
+  defp evaluate_numbers(num), do: num
 
   def build(file_name) do
     result =
--- PhoenixZero/fizzbuzz/fizz_buzz/test/fizz_buzz_test.exs
+++ PhoenixZero/fizzbuzz/fizz_buzz/test/fizz_buzz_test.exs
@@ -5,12 +5,12 @@ defmodule FizzBuzzTest do
   describe "build/1" do
     # when return pattern
     test "when a valid file is provided, return the converted list" do
-      expected_response = [1, 2, :fizz, 4, :buzz, 56, :fizz, :fizzbuzz, :buzz, :fizzbuzz]
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, 56, :fizz, :fizzbuzz, :buzz, :fizzbuzz]}
       assert FizzBuzz.build("numbers.txt") == expected_response
     end
 
     test "when a invalid file is provided, return a message error" do
-      expected_response = "error read file: enoent"
+      expected_response = {:error, "error read file: enoent"}
       assert FizzBuzz.build("numbers.txts") == expected_response
     end
   end
