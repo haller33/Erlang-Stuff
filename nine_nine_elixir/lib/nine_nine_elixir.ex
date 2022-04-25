bb13b7409265ff7d00fea190317257e09ccfc30f
haller33 <amnesia1101101@gmail.com>
2022-04-25 02:16:10 -0300

    new ninenine day01 with tests

--- nine_nine_elixir/lib/nine_nine_elixir.ex
+++ nine_nine_elixir/lib/nine_nine_elixir.ex
@@ -15,4 +15,30 @@ defmodule NineNineElixir do
   def hello do
     :world
   end
+
+  @doc """
+  The Ninety-Nine Lisp Problems
+
+  ## Working with lists
+
+   P01 (*) Find the last box of a list.
+   Example:
+   * (my-last '(a b c d))
+   (D)
+
+  """
+  def day01 do
+    my_last([:a, :b, :c, :d])
+  end
+
+  def day01(some_list) do
+    my_last(some_list)
+  end
+
+  defp my_last(list) do
+    case tl(list) do
+      [] -> hd(list)
+      _  -> my_last(tl(list))
+    end
+  end
 end
