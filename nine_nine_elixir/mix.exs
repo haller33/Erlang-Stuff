bbca8a13969cdea761663655badca6d1283dbc8e
haller33 <amnesia1101101@gmail.com>
2022-04-25 02:16:10 -0300

    new ninenine day01 with tests

--- nine_nine_elixir/mix.exs
+++ nine_nine_elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule NineNineElixir.MixProject do
     [
       app: :nine_nine_elixir,
       version: "0.1.0",
-      elixir: "~> 1.14-dev",
+      elixir: "~> 1.13-dev",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
--- nine_nine_elixir/test/nine_nine_elixir_test.exs
+++ nine_nine_elixir/test/nine_nine_elixir_test.exs
@@ -5,4 +5,14 @@ defmodule NineNineElixirTest do
   test "greets the world" do
     assert NineNineElixir.hello() == :world
   end
+
+  test "day one test day01/0" do
+    assert NineNineElixir.day01() == :d
+  end
+  
+  test "day one test day01/1" do
+    assert NineNineElixir.day01([:a, :b]) == :b
+    assert NineNineElixir.day01([:a, :b, :c]) == :c
+    assert NineNineElixir.day01([:a, [:n, :asdf], :c]) == :c
+  end
 end
