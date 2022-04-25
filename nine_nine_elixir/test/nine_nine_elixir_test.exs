1e4790efc08c560b434ea4ca54d39e09e980421b
haller33 <amnesia1101101@gmail.com>
2022-04-25 02:29:36 -0300

    my 99 day 02 elixir

--- nine_nine_elixir/test/nine_nine_elixir_test.exs
+++ nine_nine_elixir/test/nine_nine_elixir_test.exs
@@ -7,12 +7,24 @@ defmodule NineNineElixirTest do
   end
 
   test "day one test day01/0" do
-    assert NineNineElixir.day01() == :d
+    assert NineNineElixir.day01() == [:d]
   end
   
   test "day one test day01/1" do
-    assert NineNineElixir.day01([:a, :b]) == :b
-    assert NineNineElixir.day01([:a, :b, :c]) == :c
-    assert NineNineElixir.day01([:a, [:n, :asdf], :c]) == :c
+    assert NineNineElixir.day01([:a, :b]) == [:b]
+    assert NineNineElixir.day01([:a, :b, :c]) == [:c]
+    assert NineNineElixir.day01([:a, [:n, :asdf], :c]) == [:c]
+  end
+
+  test "day one test day02/0" do
+    assert NineNineElixir.day02() == [:c, :d]
+  end
+  
+  test "day two test day02/1" do
+    assert NineNineElixir.day02([]) == []
+    assert NineNineElixir.day02([:a]) == [:a]
+    assert NineNineElixir.day02([:a, :b]) == [:a, :b]
+    assert NineNineElixir.day02([:a, :b, :c]) == [:b,:c]
+    assert NineNineElixir.day02([:a, [:n, :asdf], :c]) == [[:n, :asdf], :c]
   end
 end
