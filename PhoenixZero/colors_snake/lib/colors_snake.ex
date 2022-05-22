5bf2b9adfcf1be252931626391193b22b4dba524
haller33 <amnesia1101101@gmail.com>
2022-05-22 16:38:02 -0300

    colors snake game add

---
+++ PhoenixZero/colors_snake/lib/colors_snake.ex
@@ -0,0 +1,18 @@
+defmodule ColorsSnake do
+  @moduledoc """
+  Documentation for `ColorsSnake`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ColorsSnake.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ PhoenixZero/colors_snake/test/colors_snake_test.exs
@@ -0,0 +1,8 @@
+defmodule ColorsSnakeTest do
+  use ExUnit.Case
+  doctest ColorsSnake
+
+  test "greets the world" do
+    assert ColorsSnake.hello() == :world
+  end
+end
