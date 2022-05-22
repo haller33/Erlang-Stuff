e7bfc413b3c9a87552f78e6105c3a2f62fde2a7b
haller33 <amnesia1101101@gmail.com>
2022-05-22 16:38:24 -0300

    refactory some code

--- PhoenixZero/ex_mon/lib/ex_mon.ex
+++ PhoenixZero/ex_mon/lib/ex_mon.ex
@@ -13,18 +13,17 @@ defmodule ExMon do
 
   """
 
-		@computer_name "Helmet"
-		
+  @computer_name "Helmet"
+
   alias ExMon.{Player, Game}
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
   end
 
-
-		def start_game(player) do
-				@computer_name
-				|> create_player(:punch, :kick, :heal)
-				|> Game.start(player)
-		end
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
+  end
 end
--- PhoenixZero/ex_mon/lib/game.ex
+++ PhoenixZero/ex_mon/lib/game.ex
@@ -1,21 +1,18 @@
 defmodule ExMon.Game do
-		use Agent
+  use Agent
 
-		def start(computer, player) do
+  def start(computer, player) do
+    initial_value = %{
+      computer: computer,
+      player: player,
+      turn: :player,
+      status: :started
+    }
 
-				initial_value = %{
-						computer: computer,
-						player: player,
-						turn: :player,
-						status: :started
-				}
+    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  end
 
-				Agent.start_link(fn -> initial_value end, name: __MODULE__)
-		end
-
-		def info do
-
-				Agent.get(__MODULE__, & &1)
-		end
-
+  def info do
+    Agent.get(__MODULE__, & &1)
+  end
 end
